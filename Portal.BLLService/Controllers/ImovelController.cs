f911daa259e041f9ab2d7308df6fb4d0e958db33
Bruno <82131199+BrunoSXR10@users.noreply.github.com>
2023-06-06 19:53:54 -0300

    GET imoveis

---
+++ Portal.BLLService/Controllers/ImovelController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Portal.MODEL;
+using Portal.BLL;
+
+
+namespace Portal.BLLService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImovelController : ControllerBase
+    {
+        [HttpGet(Name = "GetImovel")]
+
+        public ActionResult<List<Imovel>> GetImovel()
+        {
+            try
+            {
+                List<Imovel> list = ImovelRepository.GetAll();
+                if (list != null) { return Ok(list); }
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
