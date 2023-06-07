703b01433d09da80a55aca26ec071939d51bb9e6
João Marcelo Costa Miranda <jmcostamiranda@hotmail.com>
2023-06-07 16:16:09 -0300

    inicio do consumo da api

--- Portal.APP/telaCadastroImoveis.cs
+++ Portal.APP/telaCadastroImoveis.cs
@@ -8,7 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Portal.BLL;
+using System.Net.Http;
+using Newtonsoft.Json;
 using Portal.MODEL;
+using System.Threading.Tasks;
+using Portal.BLLService;
+using Portal.BLLService.Controllers;
 
 namespace Portal.APP
 {
@@ -31,15 +36,46 @@ namespace Portal.APP
             this.Hide();
         }
 
-        private void cadastrarButton_Click(object sender, EventArgs e)
+        private async void cadastrarButton_Click(object sender, EventArgs e)
         {
             //falta ligacao com o corretor
             Imovel i = new Imovel();
             i.Descricao = descricaoTextBox.Text;
             i.Valor = float.Parse(valorTextBox.Text);
             i.Localizacao = localizacaoTextBox.Text;
-            ImovelRepository.Add(i);
-            MessageBox.Show("Imovel Cadastrado com Sucesso!");
+            string objetoJson = JsonConvert.SerializeObject(i);
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Definir a URL da API para a qual você deseja enviar os dados
+                    string url = "http://localhost:5205/api/Imovel";
+
+                    // Criar o conteúdo da solicitação HTTP com o objeto JSON
+                    HttpContent content = new StringContent(objetoJson, Encoding.UTF8, "application/json");
+
+                    // Enviar a solicitação POST para a API
+                    HttpResponseMessage response = await client.PostAsync(url, content);
+
+                    // Verificar se a solicitação foi bem-sucedida
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // Cadastro realizado com sucesso
+                        MessageBox.Show("Objeto cadastrado com sucesso!");
+                    }
+                    else
+                    {
+                        // Lidar com erros da API
+                        MessageBox.Show("Erro ao cadastrar objeto. Resposta da API: " + response.StatusCode);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Lidar com erros de conexão ou outros erros
+                    MessageBox.Show("Erro ao cadastrar objeto: " + ex.Message);
+                }
+            }
+
         }
 
     }
