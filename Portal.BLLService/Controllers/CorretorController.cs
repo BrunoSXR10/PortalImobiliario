6029322e4106d2f4d5ec39d2b39b5c42c5cd2ea2
Bruno <82131199+BrunoSXR10@users.noreply.github.com>
2023-06-07 16:39:40 -0300

    Add corretor

---
+++ Portal.BLLService/Controllers/CorretorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Portal.BLL;
+using Portal.MODEL;
+
+namespace Portal.BLLService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CorretorController : ControllerBase
+    {
+        
+        
+    }
+}
