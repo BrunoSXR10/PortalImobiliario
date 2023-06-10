d8c04a9e2f0500c20c34b32dffabc59790447578
Bruno <82131199+BrunoSXR10@users.noreply.github.com>
2023-06-10 20:56:07 -0300

    Alteração no gitignore

--- Portal.BLLService/Controllers/CorretorController.cs
+++ Portal.BLLService/Controllers/CorretorController.cs
@@ -9,7 +9,41 @@ namespace Portal.BLLService.Controllers
     [ApiController]
     public class CorretorController : ControllerBase
     {
-        
-        
+
+        [HttpGet("{Id}", Name = "GetCorretorByID")]
+
+        public ActionResult<Corretor> GetCorretorByID(int Id)
+        {
+            try
+            {
+                Corretor _corretor = CorretorRepository.GetByID(Id);
+                if (_corretor != null)
+                {
+                    return Ok(_corretor);
+                }
+                return NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost(Name = "AddCorretor")]
+        public IActionResult AddCorretor([FromBody] Corretor corretor) //ActionResult<Imovel>
+        {
+            try
+            {
+                CorretorRepository.Add(corretor);
+                return CreatedAtAction(nameof(GetCorretorByID),
+                    new { id = corretor.Id },
+                    corretor);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
     }
 }
