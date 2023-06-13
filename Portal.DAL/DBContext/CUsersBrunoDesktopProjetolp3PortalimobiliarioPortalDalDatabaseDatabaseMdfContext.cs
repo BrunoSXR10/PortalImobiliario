7cec8a0f253b42ce15947d60d948aaa4e02f5095
João Marcelo Costa Miranda <jmcostamiranda@hotmail.com>
2023-06-12 21:37:07 -0300

    ajuste

--- Portal.DAL/DBContext/CUsersBrunoDesktopProjetolp3PortalimobiliarioPortalDalDatabaseDatabaseMdfContext.cs
+++ Portal.DAL/DBContext/CUsersBrunoDesktopProjetolp3PortalimobiliarioPortalDalDatabaseDatabaseMdfContext.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
-namespace Portal.DAL.DBContext;
+namespace Portal.MODEL;
 
 public partial class CUsersBrunoDesktopProjetolp3PortalimobiliarioPortalDalDatabaseDatabaseMdfContext : DbContext
 {
@@ -17,11 +17,6 @@ public partial class CUsersBrunoDesktopProjetolp3PortalimobiliarioPortalDalDatab
 
     public virtual DbSet<Imv> Imvs { get; set; }
 
-    public virtual DbSet<TbCorretor> TbCorretors { get; set; }
-
-    public virtual DbSet<TbEndereco> TbEnderecos { get; set; }
-
-    public virtual DbSet<TbImovel> TbImovels { get; set; }
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
@@ -48,80 +43,6 @@ public partial class CUsersBrunoDesktopProjetolp3PortalimobiliarioPortalDalDatab
             entity.Property(e => e.Valor).HasColumnName("valor");
         });
 
-        modelBuilder.Entity<TbCorretor>(entity =>
-        {
-            entity.HasKey(e => e.IdCorretor).HasName("pk_corretor");
-
-            entity.ToTable("tb_corretor");
-
-            entity.Property(e => e.IdCorretor)
-                .ValueGeneratedNever()
-                .HasColumnName("ID_corretor");
-            entity.Property(e => e.Cpf).HasColumnName("cpf");
-            entity.Property(e => e.Deputado)
-                .HasMaxLength(15)
-                .IsUnicode(false)
-                .HasColumnName("deputado");
-            entity.Property(e => e.DtNascimento)
-                .HasColumnType("date")
-                .HasColumnName("dt_nascimento");
-            entity.Property(e => e.Nome)
-                .HasMaxLength(70)
-                .IsUnicode(false)
-                .HasColumnName("nome");
-            entity.Property(e => e.Senha)
-                .HasMaxLength(15)
-                .IsUnicode(false)
-                .HasColumnName("senha");
-        });
-
-        modelBuilder.Entity<TbEndereco>(entity =>
-        {
-            entity.HasKey(e => e.IdEndereco).HasName("pk_endereco");
-
-            entity.ToTable("tb_endereco");
-
-            entity.Property(e => e.IdEndereco)
-                .ValueGeneratedNever()
-                .HasColumnName("ID_endereco");
-            entity.Property(e => e.Cep).HasColumnName("cep");
-            entity.Property(e => e.Complemento)
-                .HasMaxLength(30)
-                .IsUnicode(false)
-                .HasColumnName("complemento");
-            entity.Property(e => e.Logradoro)
-                .HasMaxLength(10)
-                .IsUnicode(false)
-                .HasColumnName("logradoro");
-            entity.Property(e => e.Numero).HasColumnName("numero");
-        });
-
-        modelBuilder.Entity<TbImovel>(entity =>
-        {
-            entity.HasKey(e => e.IdImovel).HasName("pk_imovel");
-
-            entity.ToTable("tb_imovel");
-
-            entity.Property(e => e.IdImovel)
-                .ValueGeneratedNever()
-                .HasColumnName("ID_imovel");
-            entity.Property(e => e.Descricao)
-                .HasMaxLength(150)
-                .IsUnicode(false);
-            entity.Property(e => e.IdCorretor).HasColumnName("ID_corretor");
-            entity.Property(e => e.IdEndereco).HasColumnName("ID_endereco");
-            entity.Property(e => e.Valor).HasColumnName("valor");
-
-            entity.HasOne(d => d.IdCorretorNavigation).WithMany(p => p.TbImovels)
-                .HasForeignKey(d => d.IdCorretor)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("fk_imovel_ccorretor");
-
-            entity.HasOne(d => d.IdEnderecoNavigation).WithMany(p => p.TbImovels)
-                .HasForeignKey(d => d.IdEndereco)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("fk_imovel_endereco");
-        });
 
         modelBuilder.Entity<Usuario>(entity =>
         {
